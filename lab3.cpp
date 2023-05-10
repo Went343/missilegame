b5760b89744b331f3c2bf31fa4bc89af8540c841
William_Ent <104049857+Went343@users.noreply.github.com>
2023-05-10 10:42:50 -0700

    Add files via upload

--- lab3.cpp
+++ lab3.cpp
@@ -21,6 +21,7 @@ int totalPlayers = 0;
 bool gameOver = false;
 random_device rd;
 mt19937 gen(rd());
+pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
 
 void print() {
 
@@ -114,13 +115,12 @@ void* playerTurn(void * playerID) {
 
 	int team = 0;
 	int id = (int)(long)playerID;
-	fflush(stdout);
 
 	if (id < t1) {
 
 		team = 3;
 
-	} else if (id < t2 && id >= t1) {
+	} else if (id < (t2+t1) && id >= t1) {
 
 		team = 4;
 
@@ -140,46 +140,79 @@ void* playerTurn(void * playerID) {
 
 		} else {
 
+			bool shot = false;
 
-			if (gameBoard[randRow][randCol] != team || gameBoard[randRow][randCol] != team - 2) {
+			if (gameBoard[randRow][randCol] == 1 || gameBoard[randRow][randCol] == 2) {
 
-				gameBoard[randRow][randCol] = team;
+				//Do nothing since they're starting positions
+
+			} else if (gameBoard[randRow][randCol] == team) {
+
+				gameBoard[randRow][randCol] = 0;
 
 			} else {
 
-				gameBoard[randRow][randCol] = 0;
+				shot = true;
+				gameBoard[randRow][randCol] = team;
 
 			}
 
 			int a = 0;
 			int b = 0;
-			int blank = 0;
 
-			for (int i = 0; i < 3; i++) {
+			if (shot) {
 
-				for (int j = 0; j < 3; j++) {
+				for (int i = max(0, randRow - 1); i <= min(rows - 1, randRow + 1); i++) {
 
-					if (randRow != 0 && i != 0) {
+					for (int j = max(0, randCol - 1); j <= min(cols - 1, randCol + 1); j++) {
 
-						if (randRow != rows-1 && i != 2) {
+						if (gameBoard[i][j] == 1 || gameBoard[i][j] == 3) {
 
-							if (randCol != 0 && j != 0) {
+							a += 1;
 
-								if (randCol != cols-1 && j != 2) {
+						} else if (gameBoard[i][j] == 2 || gameBoard[i][j] == 4) {
 
-									if (gameBoard[randRow + i - 1][randCol + j - 1] == 1 || gameBoard[randRow + i - 1][randCol + j - 1] == 3) {
+							b += 1;
 
-										a += 1;
+						}
 
-									} else if (gameBoard[randRow + i - 1][randCol + j - 1] == 2 || gameBoard[randRow + i - 1][randCol + j - 1] == 4) {
+					}
 
-										b += 1;
+				}
 
-									} else {
+				cout << "a:" << a << " b:" << b << endl;
 
-										blank += 1;
+				if (a > b) {
 
-									}
+					for (int i = max(0, randRow - 1); i <= min(rows - 1, randRow + 1); i++) {
+
+						for (int j = max(0, randCol - 1); j <= min(cols - 1, randCol + 1); j++) {
+
+							if (gameBoard[i][j] == 4 || gameBoard[i][j] == 0) {
+
+								if (team == 3) {
+
+									gameBoard[i][j] = 3;
+
+								}
+
+							}
+
+						}
+
+					}
+
+				} else if (b > a) {
+
+					for (int i = max(0, randRow - 1); i <= min(rows - 1, randRow + 1); i++) {
+
+						for (int j = max(0, randCol - 1); j <= min(cols - 1, randCol + 1); j++) {
+
+							if (gameBoard[i][j] == 3 || gameBoard[i][j] == 0) {
+
+								if (team == 4) {
+
+									gameBoard[i][j] = 4;
 
 								}
 
@@ -193,18 +226,9 @@ void* playerTurn(void * playerID) {
 
 			}
 
-
-			if (a > b && a > blank) {
-
-				//Fill all spaces with a
-
-			} else if (b > a && b > blank) {
-
-				//Fill all spaces with b
-
-			}
-
+			pthread_mutex_lock(&myMutex);
 			print();
+			pthread_mutex_unlock(&myMutex);
 
 		}
 
@@ -212,6 +236,8 @@ void* playerTurn(void * playerID) {
 		sleep(slp(gen));
 
 	}
+
+	fflush(stdout);
 
 	return (void*)0;
 
@@ -257,14 +283,7 @@ void* supervisor(void * superID) {
 
 					continue;
 
-				} else {
-
-					system("Color 07");
-					cout << "An error has occurred. Please try again." << endl;
-					exit(-1);
-
 				}
-
 			}
 
 		}
@@ -275,14 +294,7 @@ void* supervisor(void * superID) {
 
 		}
 
-		sleep(3);
-
 	}
-
-
-	sleep(4);
-
-	winCheck();
 
 	return (void*)0;
 }
@@ -300,16 +312,6 @@ int main(int argc, char* argv[]) {
 	rows = atoi(argv[3]);
 	cols = atoi(argv[4]);
 	totalPlayers = t1 + t2;
-
-	int playerIDs[totalPlayers];
-	int a = 1;
-
-	for (int i = 0; i < totalPlayers; i++) {
-
-		playerIDs[i] = a;
-		a++;
-
-	}
 
 	pthread_t playerThreads[totalPlayers + 1];
 
@@ -330,17 +332,56 @@ int main(int argc, char* argv[]) {
 
 	for (int i = 0; i < totalPlayers; i++) {
 
+		bool placed = false;
+
+		while (!placed) {
+
+			uniform_int_distribution<> disR(0, rows-1);
+			int randRow = disR(gen);
+			uniform_int_distribution<> disC(0, cols-1);
+			int randCol = disC(gen);
+
+			if (gameBoard[randRow][randCol] == 0) {
+
+				if (i < t1) {
+
+					placed = true;
+					gameBoard[randRow][randCol] = 1;
+
+				} else if (i < (t1+t2) && i >= t1) {
+
+					placed = true;
+					gameBoard[randRow][randCol] = 2;
+
+				}
+
+			}
+
+		}
+
+	}
+
+	for (int i = 0; i < totalPlayers; i++) {
+
 		pthread_create(&playerThreads[i], 0, playerTurn, (void *)(long)i);
 
 	}
 
 	pthread_create(&playerThreads[totalPlayers], 0, supervisor, (void *)(long)totalPlayers);
 
+	while(!gameOver) {
+
+		sleep(1);
+
+	}
+
 	for(long i = 0; i == totalPlayers; i++) {
 
 		pthread_join(playerThreads[i],NULL);
 
 	}
+
+	winCheck();
 
 	return 0;
 }
